74e252d6e23461b67fc85621418ab139e7d23a8e
xianwei li <xil148@pitt.edu>
2017-09-19 10:19:49 -0400

    updated news model and item view

--- app/src/main/java/com/xianwei/news/models/News.java
+++ app/src/main/java/com/xianwei/news/models/News.java
@@ -1,41 +1,98 @@
 package com.xianwei.news.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by xianwei li on 8/14/2017.
  */
 
-public class News {
+public class News implements Parcelable {
     private String title;
-    private String author;
-    private String section;
+    private String description;
+    private String urlString;
+    private String imageUrlString;
     private String date;
-    private String webUrl;
 
-    public News(String title, String author, String section, String date, String webUrl) {
+    public News(String title, String description, String urlString, String imageUrlString, String date) {
         this.title = title;
-        this.author = author;
-        this.section = section;
+        this.description = description;
+        this.urlString = urlString;
+        this.imageUrlString = imageUrlString;
         this.date = date;
-        this.webUrl = webUrl;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getDescription() {
+        return description;
     }
 
-    public String getSection() {
-        return section;
+    public String getUrlString() {
+        return urlString;
+    }
+
+    public String getImageUrlString() {
+        return imageUrlString;
     }
 
     public String getDate() {
         return date;
     }
 
-    public String getWebUrl() {
-        return webUrl;
+    public void setTitle(String title) {
+        this.title = title;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUrlString(String urlString) {
+        this.urlString = urlString;
+    }
+
+    public void setImageUrlString(String imageUrlString) {
+        this.imageUrlString = imageUrlString;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.description);
+        dest.writeString(this.urlString);
+        dest.writeString(this.imageUrlString);
+        dest.writeString(this.date);
+    }
+
+    protected News(Parcel in) {
+        this.title = in.readString();
+        this.description = in.readString();
+        this.urlString = in.readString();
+        this.imageUrlString = in.readString();
+        this.date = in.readString();
+    }
+
+    public static final Parcelable.Creator<News> CREATOR = new Parcelable.Creator<News>() {
+        @Override
+        public News createFromParcel(Parcel source) {
+            return new News(source);
+        }
+
+        @Override
+        public News[] newArray(int size) {
+            return new News[size];
+        }
+    };
 }
